f55c3063b6f9c2055ee442b0d154a429505c58c7
subh <subhajitchaudhury05@gmail.com>
2026-01-31 01:48:28 +0530

    feat : added a extra test wallet

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,4 @@
-import { createSolanaRpc, createSolanaRpcSubscriptions, createKeyPairSignerFromBytes } from '@solana/kit';
+import { createSolanaRpc, createSolanaRpcSubscriptions, createKeyPairSignerFromBytes, generateKeyPairSigner, airdropFactory, lamports } from '@solana/kit';
 import { readFileSync } from 'fs';
 import { homedir } from 'os';
 
@@ -6,6 +6,7 @@ export type Client = {
     rpc: ReturnType<typeof createSolanaRpc>;
     rpcSubscriptions: ReturnType<typeof createSolanaRpcSubscriptions>;
     wallet: Awaited<ReturnType<typeof createKeyPairSignerFromBytes>>;
+    testWallet: Awaited<ReturnType<typeof generateKeyPairSigner>>;
 };
 
 let client: Client | undefined;
@@ -14,6 +15,7 @@ export async function createClient(): Promise<Client> {
         // 🌐 Using DEVNET
         const rpc = createSolanaRpc('https://api.devnet.solana.com');
         const rpcSubscriptions = createSolanaRpcSubscriptions('wss://api.devnet.solana.com');
+        const airdrop = airdropFactory({ rpc, rpcSubscriptions } as any); 
 
         // Load existing wallet from Solana CLI config (already has funds!)
         const keypairPath = `${homedir()}/.config/solana/id.json`;
@@ -22,10 +24,19 @@ export async function createClient(): Promise<Client> {
         
         console.log(`🔑 Loaded wallet from ${keypairPath}`);
 
+        // generating a extra wallet for testing 
+        const testWallet = await generateKeyPairSigner();
+        await airdrop({
+            recipientAddress : testWallet.address,
+            lamports : lamports(500_000_000n),
+            commitment : 'confirmed',
+        })
+
         client = {
             rpc,
             rpcSubscriptions,
             wallet,
+            testWallet,
         };
     }
     return client;
